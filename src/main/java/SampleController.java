1049c9206deafa7167141aa5567b5c7f16e4a747
Shubhangi Gupta <shubhangigupta1910@gmail.com>
2018-04-14 18:54:31 -0700

    Add simple project pom with spring boot dependencies and hello world controller

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,21 @@
+package com.practice.rent;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String test() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
