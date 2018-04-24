28dd5224ec6f11d93bea7e380f9d0eeb0023c345
Shubhangi Gupta <shubhangigupta1910@gmail.com>
2018-04-23 20:33:02 -0700

    Remove border

--- src/main/java/com/startup/model/User.java
+++ src/main/java/com/startup/model/User.java
@@ -24,7 +24,7 @@ public class User {
         StringBuilder buf = new StringBuilder();
         buf.append("<html>" +
                 "<body>" +
-                "<table border='1'>" +
+                "<table>" +
                 "<tr>" +
                 "<th>UserID</th>" +
                 "<th>Email Address</th>" +
