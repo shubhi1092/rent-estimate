156835cd8ded694147b7e7da99ee431f4745209d
Shubhangi Gupta <shubhangigupta1910@gmail.com>
2018-04-24 18:05:53 -0700

    Fixing script

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -2,10 +2,10 @@
 $('form[name="signupform"]').submit(function(){
 
     var obj = new Object();
-    obj.firstName = ('input[name="firstName"]').val();
-    obj.lastName = ('input[name="lastName"]').val();
-    obj.emailAddress = ('input[name="emailAddress"]').val();
-    obj.phoneNumber = ('input[name="phoneNumber"]').val();
+    obj.firstName = $('input[name="firstName"]').val();
+    obj.lastName = $('input[name="lastName"]').val();
+    obj.emailAddress = $('input[name="emailAddress"]').val();
+    obj.phoneNumber = $('input[name="phoneNumber"]').val();
 
     $.ajax({
         type: 'POST',
